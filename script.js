e3a8dba5e595d2e4144ed101fb6b880c42d253ae
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2022-12-29 16:48:52 +0700

    Add a div for the grid

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const container = document.querySelector('#container');
+
+const grid = document.createElement('div');
+grid.classList.add('grid');
+grid.textContent = 'hello';
+container.appendChild(grid);
+
+
