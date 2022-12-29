de78dcea3a68e3349f282c7aa8f5506cb6edbda1
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2022-12-29 19:18:52 +0700

    Set size of grid container and squares to make the grid display as 16x16

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 
 const container = document.querySelector('#container');
 
-const grid = document.createElement('div');
-grid.classList.add('grid');
-container.appendChild(grid);
 
 function grid16() {
   for(let i = 0; i < 256; i++) {
-   const grid = document.createElement('div');                
-    grid.className = 'grid';
-    container.appendChild(grid);       
+   const grid = document.createElement('div');
+   grid.classList.add('grid');
+   container.appendChild(grid);      
 }
 }
 
