4351680eb1aa4ee635843e2a4fe78dfe07566a7f
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2022-12-29 22:12:36 +0700

    Add grids for 64x64 and 96x96

--- script.js
+++ script.js
@@ -19,11 +19,33 @@ function grid32() {
     }
   }
   
-grid32();
+//grid32();
+
+function grid64() {
+    for(let i = 0; i < 4096; i++) {
+     const box64 = document.createElement('div');
+     box64.classList.add('box64');
+     container.appendChild(box64);      
+    }
+  }
+  
+//grid64();
+
+function grid96() {
+    for(let i = 0; i < 9216; i++) {
+     const box96 = document.createElement('div');
+     box96.classList.add('box96');
+     container.appendChild(box96);      
+    }
+  }
+  
+grid96();
 
 
 box16 = document.querySelectorAll('.box16');
 box32 = document.querySelectorAll('.box32');
+box64 = document.querySelectorAll('.box64');
+box96 = document.querySelectorAll('.box96');
 
 box16.forEach((box16) => {
     box16.addEventListener('mouseover', () => {
@@ -38,3 +60,14 @@ box32.forEach((box32) => {
     });
 });
 
+box64.forEach((box64) => {
+    box64.addEventListener('mouseover', () => {
+    box64.style.backgroundColor = 'rgba(171, 122, 217, 0.408)';
+    });
+});
+
+box96.forEach((box96) => {
+    box96.addEventListener('mouseover', () => {
+    box96.style.backgroundColor = 'rgba(171, 122, 217, 0.408)';
+    });
+});
