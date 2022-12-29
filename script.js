33f5da9e25aab4c7785324b361587ecfd13294b6
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2022-12-29 17:25:58 +0700

    Add function to create a grid with 256 squares

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ const container = document.querySelector('#container');
 
 const grid = document.createElement('div');
 grid.classList.add('grid');
-grid.textContent = 'hello';
 container.appendChild(grid);
 
+function grid16() {
+  for(let i = 0; i < 256; i++) {
+   const grid = document.createElement('div');                
+    grid.className = 'grid';
+    container.appendChild(grid);       
+}
+}
 
+grid16();
